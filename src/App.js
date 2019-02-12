b663b38a957d0bb2e6de20f86903f44e594c3665
Tim Corr <tbcorr@gmail.com>
2019-02-11 19:29:58 -0500

    setup basic routing for registration & login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import Register from './Register';
+import Login from './Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
       </div>
     );
   }
